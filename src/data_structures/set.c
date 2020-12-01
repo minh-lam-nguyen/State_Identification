646ebdc938fb66cb262853a1e4bb88db355cc2bc
Dimitri Watel <watel.dimitri@gmail.com>
2020-12-01 15:20:00 +0100

    Refactor:
    - add bin and obj directories
    - add data_structure directory
    - change makefile
    - make size parameter in initialization of set

--- src/data_structures/set.c
+++ src/data_structures/set.c
@@ -8,6 +8,7 @@
 struct s_set{
     int* s[HASH_SIZE][SIZE_V]; // Tableau contenant HASH_SIZE liste de SIZEV éléments
     int c[HASH_SIZE]; // Nombre déléments avec chaque hash dans le set
+    int size; // Nombre minimum d'entiers dans les int* ajoutés dans le set
 };
 
 /**
@@ -32,11 +33,12 @@ int equals(int* e, int* f, int size){
 /**
  * Initialisation du Set
  */
-Set initSet(){
+Set initSet(int size){
     Set s = (Set) malloc(sizeof(struct s_set));
     for (int j=0; j<HASH_SIZE; j++) {
         s->c[j] = 0;
     }
+    s->size = size;
     return s;
 }
 
@@ -52,11 +54,11 @@ void freeSet(Set s){
  * e doit être un pointeur sur au moins size entiers créé avec malloc
  * Attention, modifier e après coup peut engendrer des erreurs lors de la recherche ou de l'ajout futur d'éléments.
  */
-int add(Set s, int* e, int size){
-    int h = hash(e, size);
+int add(Set s, int* e){
+    int h = hash(e, s->size);
     int c = s->c[h];
     for(int i = 0; i < c; i++)
-        if(equals(e, s->s[h][i], size))
+        if(equals(e, s->s[h][i], s->size))
             return 0;
 
     if(c == SIZE_V)
@@ -71,11 +73,11 @@ int add(Set s, int* e, int size){
  * Vérifie si e est dans s. Renvoie 1 si oui et 0 sinon.
  * e doit être un pointeur sur au moins size entiers
  */
-int find(Set s, int* e, int size){
-    int h = hash(e, size);
+int find(Set s, int* e){
+    int h = hash(e, s->size);
     int c = s->c[h];
     for(int i = 0; i < c; i++){
-        if(equals(e, s->s[h][i], size))
+        if(equals(e, s->s[h][i], s->size))
             return 1;
     }
     return 0;
